cb8d0eab0f79218e0beead554cd7218538c3bc3a
Long <146718079+kimlong-vtech@users.noreply.github.com>
2024-07-14 18:26:29 +0700

    resolve init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,42 +1,29 @@
+ 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.g.lazygit_floating_window_scaling_factor = 1
 
--- require "options"
--- require "mappings"
--- require "commands"
+require "options"
+require "mappings"
+require "commands"
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+-- bootstrap plugins & lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim" -- path where its going to be installed
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+require "plugins"
 
-
-
- require "plugins"
-
--- vim.cmd "colorscheme darkplus"
-
--- Set GIT_EDITOR to use nvr if Neovim and nvr are available
-if vim.fn.has('nvim') == 1 and vim.fn.executable('nvr') == 1 then
-  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-end
+vim.cmd "colorscheme darkplus"
